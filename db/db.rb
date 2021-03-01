f8f6f4e7492d1dcae5c474f0d28380858c951c2a
Milosz Tabaka <mmtabaka1@sheffield.ac.uk>
2021-03-01 14:14:47 +0000

    Setting up the database

--- db/db.rb
+++ db/db.rb
@@ -1,1 +1,14 @@
-puts "branch test"
+require "logger"
+require "sequel"
+
+type = ENV.fetch("APP_ENV", "users")
+
+db_path = File.dirname(__FILE__)
+db = "#{db_path}/#{type}.sqlite3"
+
+log_path = "#{File.dirname(__FILE__)}/../log/"
+log = "#{log_path}/#{type}.log"
+
+Dir.mkdir(log_path) unless File.exist?(log_path)
+
+DB = Sequel.sqlite(db, logger: Logger.new(log))

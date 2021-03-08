a0caebe0d2d037b3a05dfba9203ddd2cf4f29ef8
Vykintas Valuzis <vvaluzis1@sheffield.ac.uk>
2021-03-08 22:40:07 +0200

    Added register() method and its spec

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -1,3 +1,4 @@
 get "/" do
-    
+  u = M_user.register("new@email.com", "Pass", 1)
+  u.to_s
 end
--- models/model.rb
+++ models/model.rb
@@ -1,3 +1,17 @@
+require 'sequel'
+
 class M_user < Sequel::Model
-    
+
+  # Checks if user exists and creates a new one if not
+  # @param email email address in valid format
+  # @param password plaintext password
+  # @param user_type type of the user
+  # @return new instance or nil if user already exists
+  def self.register(email, password, user_type)
+    if M_user.where(email: email).single_record.nil?
+      return M_user.create(email: email, password: password, user_type: user_type)
+    end
+
+    nil
+  end
 end

4b802d21c0f8cf579f8e402e90cbe8a53dccdd99
Milosz Tabaka <mmtabaka1@sheffield.ac.uk>
2021-03-09 18:42:47 +0000

    Login with provisional session controllers

--- controllers/login.rb
+++ controllers/login.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+get "/" do
+    redirect "/login" unless session[:logged_in]
+    erb :index
+end
+
 get "/login" do
     erb :login
 end
@@ -9,6 +14,12 @@ post "/login" do
     @password = params["password"]
     
     if M_user.login(@email, @password)
-        redirect "/home"
+        session[:logged_in] = true
+        redirect "/"
     end
+end
+
+get "/logout" do
+  session.clear
+  erb :logout
 end

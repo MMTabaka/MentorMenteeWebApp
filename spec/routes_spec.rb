b367c4301ce10ae4c1094c7ba8861ccf21a17543
Milosz Tabaka <mmtabaka1@sheffield.ac.uk>
2021-03-22 11:19:56 +0000

    Readme and route test

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -2,4 +2,19 @@ require "rspec"
 require "rack/test"
 
 require_relative "../controllers/registration"
-require_relative "../controllers/"
+require_relative "../controllers/login"
+
+RSpec.describe "Routes Tests" do
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+  
+  describe "GET /login" do
+    it "has a status code of 200 (OK)" do
+      get "/login"
+      expect(last_response.status).to eq(200)
+    end
+  end
+end

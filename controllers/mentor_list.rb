6772286eb248c073be54e4d77b647034f518f5e5
Nathan Scott <nathan@nathanscott.co.uk>
2021-03-24 20:25:12 +0000

    Updates user sorting

--- controllers/mentor_list.rb
+++ controllers/mentor_list.rb
@@ -3,6 +3,11 @@ require_relative '../helpers/authenticated'
 
 get '/mentors' do
   authenticated
-  @mentors = User.retrieve_users(UserType::MENTOR)
+  user = User[session[:user]]
+  unsorted_mentors = User.retrieve_users(UserType::MENTOR)
+  # Calculate levenshtein distance for each mentor and store in an array
+  values = unsorted_mentors.map { |mentor| User.levenshtein_distance(mentor[:interest_areas], user[:interest_areas]) }
+  # Combines mentors and levenshtein distance values into a hash that is sorted and returned
+  @mentors = Hash[unsorted_mentors.zip(values).sort_by {|k, v| v}].keys
   erb :mentorList
 end

edf8ee315eae69e47cfe63d3f2d169e3697bd835
Vykintas Valuzis <vvaluzis1@sheffield.ac.uk>
2021-04-21 20:28:44 +0300

    Removed my email from manual_testing.rb

--- emailing/manual_testing.rb
+++ emailing/manual_testing.rb
@@ -3,8 +3,8 @@ require_relative 'password_reset_email'
 
 # Run this file with your email to test email sending manually.
 # Note: don't push your email to Git!
-YOUR_EMAIL = 'vvaluzis1@sheffield.ac.uk'
-YOUR_NAME = 'Vykintas V'
+YOUR_EMAIL = ''
+YOUR_NAME = ''
 
 to_mentor = EmailToMentor.new(YOUR_EMAIL, YOUR_NAME, 'mentee@email.com', 'John Doe')
 reset_pass = PasswordResetEmail.new(YOUR_NAME, YOUR_EMAIL, 'tempPass123')

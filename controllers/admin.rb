cf8eb9f30845bf43b8b8d93e3c9eabc25bf81019
Vykintas Valuzis <vvaluzis1@sheffield.ac.uk>
2021-04-27 00:12:51 +0300

    Added empty admin controller Added admin auth checking helper

---
+++ controllers/admin.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require_relative '../helpers/authenticated'
+
+get '/admin/' do
+  # TODO: Implement
+end
+
+get '/admin/login' do
+  # TODO: Implement
+end
--- helpers/authenticated.rb
+++ helpers/authenticated.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require_relative '../helpers/authenticated'
+
 
 def authenticated
   redirect '/login' if session[:user].nil?
+end
+
+def authenticated_as_admin
+  user_id = session[:user]
+  return redirect '/admin/login' if user_id.nil?
+
+  user = User[user_id]
+  redirect '/admin/login' unless user[:user_type] == UserType::ADMIN
 end

bc130e3379615670abde2d74bca17ed0f34f5f32
Vykintas Valuzis <vvaluzis1@sheffield.ac.uk>
2021-04-28 23:43:43 +0300

    Somewhat working admin functionality

--- controllers/admin.rb
+++ controllers/admin.rb
@@ -2,26 +2,43 @@ require 'sinatra'
 require_relative '../helpers/authenticated'
 
 get '/admin/?' do
-  # TODO: Implement
+  authenticated_as_admin
   @admin_name = 'Admin John'
   @users = User.all
   erb :admin
 end
 
 get '/admin/login/?' do
-  # TODO: Implement
-  'Admin Login Page'
+  redirect '/admin' if authenticated_as_admin?
+  # TODO: Refactor this to DRY
+  session[:is_valid] = true if session[:is_valid].nil?
+  @is_valid = session[:is_valid]
+  erb :login
+end
+
+post '/admin/login/?' do
+  @email = params['email']
+  @password = params['pass']
+  user = User.login(@email, @password)
+  if user.nil? || user[:user_type] != UserType::ADMIN
+    session[:is_valid] = false
+  else
+    # Setting user id to access logged in user later
+    session[:user] = user.id
+    session[:is_valid] = nil
+    redirect '/admin'
+  end
+  redirect '/login'
 end
 
 post '/admin/suspend/?' do
-  # TODO: change to post
-  puts params
+  authenticated_as_admin
   user = User[params['id']]
   user.toggle_suspension
 end
 
 post '/admin/reset/?' do
-  # TODO: change to post
+  authenticated_as_admin
   user = User[params['id']]
   user.reset_password
 end

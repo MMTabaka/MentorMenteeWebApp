ddaed062b84dc43f2e5211515a84c4165ddfb582
Vykintas Valuzis <vvaluzis1@sheffield.ac.uk>
2021-04-29 21:38:11 +0300

    Regenerated DB added .gitignore

--- db/database.sql
+++ db/database.sql
@@ -39,14 +39,13 @@ CREATE TABLE connections (
      rejection_reason TEXT
 );
 
-INSERT INTO users (user_type, email, password, name, department, bio, request, acceptance, suspension, interest_areas)
+INSERT INTO users (user_type, email, password, name, department, bio, suspension, interest_areas)
 VALUES (1, 'email@email.com', 'VerySecurePass1337', 'Mentee John', 'Department of Computer Science', 'Random bio text',
-        '', 0, 0, 'field1,field2,field3');
+         0, 'field1,field2,field3');
 
-INSERT INTO users (user_type, email, password, name, department, bio, request, acceptance, suspension, interest_areas)
-VALUES (0, 'email2@email.com', 'SecurePass123', 'Mentor Paul', 'Department of Nothing', 'Random bio text',
-        '', 0, 0, 'field1,field5,field6');
+INSERT INTO users (user_type, email, password, name, department, bio, suspension, interest_areas)
+VALUES (0, 'email2@email.com', 'SecurePass123', 'Mentor Paul', 'Department of Nothing', 'Random bio text', 0, 'field1,field5,field6');
 
-INSERT INTO users (user_type, email, password, name, department, bio, request, acceptance, suspension, interest_areas)
+INSERT INTO users (user_type, email, password, name, department, bio, suspension, interest_areas)
 VALUES (0, 'email3@email.com', 'VerySecurePass1337', 'Mentor Ringo', 'Department of Computer Science', 'Random bio text',
-        '', 0, 0, 'field1,field2,field3');
+        0, 'field1,field2,field3');

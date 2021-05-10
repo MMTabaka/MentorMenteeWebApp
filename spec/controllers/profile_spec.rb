3909762e0673d8d8c7b0f616d3c9d1b801cb4f5b
Nathan Scott <nathan@nathanscott.co.uk>
2021-05-10 13:03:11 +0100

    Finalised profile_spec

--- spec/controllers/profile_spec.rb
+++ spec/controllers/profile_spec.rb
@@ -27,34 +27,49 @@ RSpec.describe 'Account Tests', type: :controller do
       interest_areas: 'Area 1,Area 2'
     }
 
-    # Example param hash
-    # params = {
-    #   "name"=>"",
-    #   "email"=>"email@email.com",
-    #   "password"=>"", "re-pass"=>"",
-    #   "department"=>"Department of Computer Science",
-    #   "areas"=>["9", "21", "34"],
-    #   "bio"=>" "}
-
     User.create(base_hash)
   end
-  # after(:each) do
-  #   User.destroy
-  # end
+
+  after(:each) do
+    cleanup
+  end
+  
   describe 'GET /account', type: :controller do
-    it 'changes profile details' do
-      params = {
-        'email' => 'TEST@email.com'
-      }
-
-      # cookies['rack.session'] = cookie
-      post '/login', { 'email' => 'test4@email.com', 'pass' => 'VerySecurePass1337' }
-      cookie = last_response.cookies['rack.session']
-      post '/account', { email: 'TEST@email.com' }
-      get '/account', headers: cookie
-      expect(last_response.body).to include(params['email'])
-
-      # expect(last_response.status).to eq(200)
+    context 'when we try and change the email' do
+      it 'changes profile details' do
+        post '/login', { 'email' => 'test4@email.com', 'pass' => 'VerySecurePass1337' }
+        cookie = last_response.cookies['rack.session']
+        post '/account', { email: 'TEST@email.com' }
+        get '/account', headers: cookie
+        expect(last_response.body).to include('TEST@email.com')
+      end
+    end
+    context 'when we try and change the bio' do
+      it 'changes profile details' do
+        post '/login', { 'email' => 'test4@email.com', 'pass' => 'VerySecurePass1337' }
+        cookie = last_response.cookies['rack.session']
+        post '/account', { bio: 'Example Bio' }
+        get '/account', headers: cookie
+        expect(last_response.body).to include('Example Bio')
+      end
+    end
+    context 'when we try and change the department' do
+      it 'changes profile details' do
+        post '/login', { 'email' => 'test4@email.com', 'pass' => 'VerySecurePass1337' }
+        cookie = last_response.cookies['rack.session']
+        post '/account', { department: 'Example Department' }
+        get '/account', headers: cookie
+        expect(last_response.body).to include('Example Department')
+      end
+    end
+    context 'when we try and change the name' do
+      it 'changes profile details' do
+        post '/login', { 'email' => 'test4@email.com', 'pass' => 'VerySecurePass1337' }
+        cookie = last_response.cookies['rack.session']
+        post '/account', { name: 'Example Name' }
+        get '/account', headers: cookie
+        expect(last_response.body).to include('Example Name')
+      end
     end
   end
 end
